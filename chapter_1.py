f1106761de11c1aecb55a47c6d428f8301a43d90
Jean Nassar <jn.masasin+git@gmail.com>
2017-08-17 16:33:55 -0600

    Use pytest instead of doctest.

--- chapter_1.py
+++ chapter_1.py
@@ -12,22 +12,10 @@ COMPLEMENTS = str.maketrans(BASES, 'TGCA')
 
 @lru_cache()
 def hamming_distance(s1, s2):
-    '''
-    >>> s1 = 'GGGCCGTTGGT'
-    >>> s2 = 'GGACCGTTGAC'
-    >>> hamming_distance(s1, s2)
-    3
-
-    '''
     return sum(l != r for l, r in zip(s1, s2))
 
 
 def neighbors(pattern, dist_max, *, method=hamming_distance):
-    '''
-    >>> sorted(neighbors('ACG', 1))
-    ['AAG', 'ACA', 'ACC', 'ACG', 'ACT', 'AGG', 'ATG', 'CCG', 'GCG', 'TCG']
-
-    '''
     if dist_max == 0:
         return set([pattern])
     if len(pattern) == 1:
@@ -46,64 +34,22 @@ def neighbors(pattern, dist_max, *, method=hamming_distance):
 
 
 def pattern_count(genome, pattern, *, dist_max=0, method=hamming_distance):
-    '''
-    >>> pattern_count('GCGCG', 'GCG')
-    2
-    >>> pattern_count('AACAAGCTGATAAACATTTAAAGAG', 'AAAAA', dist_max=1)
-    4
-    >>> pattern_count('AACAAGCTGATAAACATTTAAAGAG', 'AAAAA', dist_max=2)
-    11
-
-    '''
     return sum(method(pattern, substring) <= dist_max
                for substring in window(genome, len(pattern)))
 
 
 def kmer_counts(genome, kmer_length, *, dist_max=0, method=hamming_distance,
                 reverse=False):
-    '''
-    >>> genome = 'GCGCG'
-    >>> sorted(dict(kmer_counts(genome, 2)).items())
-    [(2, ['GC', 'CG'])]
-    >>> counts = kmer_counts(genome, 2, dist_max=1)
-    >>> sorted(counts) == [2, 4]
-    True
-    >>> sorted(sorted(v) for v in counts.values())
-    [['AC', 'AG', 'CA', 'CG', 'CT', 'GA', 'GC', 'GT', 'TC', 'TG'], ['CC', 'GG']]
-    >>> counts = kmer_counts(genome, 2, dist_max=1, reverse=True)
-    >>> sorted(counts) == [4, 8]
-    True
-
-    '''
-    freqs = dict_frequencies(genome, kmer_length,
-                             dist_max=dist_max, method=method, reverse=reverse)
-    counts = defaultdict(list)
+    freqs = frequencies(genome, kmer_length,
+                        dist_max=dist_max, method=method, reverse=reverse)
+    counts = defaultdict(set)
     for k, v in freqs.items():
-        counts[v].append(k)
+        counts[v].add(k)
     return counts
 
 
 def frequent_kmers(genome, kmer_length, *, dist_max=0, method=hamming_distance,
                    reverse=False, min_freq=None):
-    '''
-    >>> genome = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
-    >>> kmer_length = 4
-    >>> sorted(frequent_kmers(genome, kmer_length))
-    ['CATG', 'GCAT']
-    >>> sorted(frequent_kmers(genome, kmer_length, min_freq=2))
-    ['ATGA', 'CATG', 'GCAT', 'TGCA']
-    >>> sorted(frequent_kmers('GCGAT', 3))
-    []
-    >>> sorted(frequent_kmers(genome, kmer_length, dist_max=1))
-    ['ATGC', 'ATGT', 'GATG']
-    >>> sorted(frequent_kmers(genome, kmer_length, dist_max=1, min_freq=4))
-    ['AATG', 'ACAT', 'AGCA', 'ATGA', 'ATGC', 'ATGG', 'ATGT', 'CATG', 'CATT', 'CGTG', 'CTTG', 'GATG', 'GCGT', 'GGAT', 'TATG', 'TCAT', 'TGAA', 'TGCT']
-    >>> sorted(frequent_kmers('GCGAT', 4, dist_max=1))
-    []
-    >>> sorted(frequent_kmers(genome, kmer_length, dist_max=1, reverse=True))
-    ['ACAT', 'ATGT']
-
-    '''
     counts = kmer_counts(genome, kmer_length, dist_max=dist_max, method=method,
                          reverse=reverse)
 
@@ -118,109 +64,51 @@ def frequent_kmers(genome, kmer_length, *, dist_max=0, method=hamming_distance,
 
 
 def pattern_to_number(pattern):
-    '''
-    >>> pattern_to_number('GT')
-    11
-    >>> pattern_to_number('AGT')
-    11
-
-    '''
     return int(DNA_BASE4.decode(pattern))
 
 
-def number_to_pattern(number, n_bases=0):
-    '''
-    >>> number_to_pattern(11)
-    'GT'
-    >>> number_to_pattern(11, n_bases=3)
-    'AGT'
-
-    '''
+def number_to_pattern(number, *, n_bases=0):
     return f'{DNA_BASE4.encode(number):A>{n_bases}}'
 
 
 def window(genome, n_bases):
-    '''
-    >>> genome = 'GCGCG'
-    >>> list(window(genome, 2))
-    ['GC', 'CG', 'GC', 'CG']
-    >>> list(window(genome, 3))
-    ['GCG', 'CGC', 'GCG']
-
-    '''
     for i in range(len(genome) - n_bases + 1):
         yield genome[i:i + n_bases]
 
 
-def list_frequencies(genome, n_bases, *, dist_max=0, method=hamming_distance):
-    '''
-    >>> genome = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
-    >>> list_frequencies(genome, 2)
-    [0, 1, 2, 4, 3, 0, 2, 1, 3, 4, 0, 2, 0, 1, 5, 1]
-    >>> genome = 'AAGCAAAGGTGGG'
-    >>> list_frequencies(genome, 2, dist_max=1)
-    [6, 6, 9, 6, 4, 2, 7, 2, 9, 5, 8, 5, 5, 2, 6, 2]
+def vectorize(frequency_dict):
+    n_bases = len(list(frequency_dict.keys())[0])
+    return [frequency_dict[number_to_pattern(i, n_bases=n_bases)]
+            for i in range(len(BASES)**n_bases)]
 
-    '''
-    frequencies = [0] * (4**n_bases)
+
+def frequencies(genome, n_bases, *, dist_max=0, method=hamming_distance,
+                reverse=False):
+    freqs = defaultdict(int)
     for substring in window(genome, n_bases):
         for neighbor in neighbors(substring, dist_max, method=method):
-            frequencies[pattern_to_number(neighbor)] += 1
-    return frequencies
-
-
-def dict_frequencies(genome, n_bases, *, dist_max=0, method=hamming_distance,
-                     reverse=False):
-    '''
-    >>> genome = 'GATTACA'
-    >>> sorted(dict(dict_frequencies(genome, 2)).items())
-    [('AC', 1), ('AT', 1), ('CA', 1), ('GA', 1), ('TA', 1), ('TT', 1)]
-    >>> sorted(dict(dict_frequencies(genome, 2, dist_max=1)).items())
-    [('AA', 5), ('AC', 2), ('AG', 2), ('AT', 3), ('CA', 3), ('CC', 2), ('CG', 1), ('CT', 3), ('GA', 3), ('GC', 2), ('GG', 1), ('GT', 3), ('TA', 4), ('TC', 3), ('TG', 2), ('TT', 3)]
-    >>> sorted(dict(dict_frequencies(genome, 2, dist_max=1, reverse=True)).items())
-    [('AA', 8), ('AC', 5), ('AG', 5), ('AT', 6), ('CA', 5), ('CC', 3), ('CG', 2), ('CT', 5), ('GA', 6), ('GC', 4), ('GG', 3), ('GT', 5), ('TA', 8), ('TC', 6), ('TG', 5), ('TT', 8)]
-
-    '''
-    frequencies = defaultdict(int)
-    for substring in window(genome, n_bases):
-        for neighbor in neighbors(substring, dist_max, method=method):
-            frequencies[neighbor] += 1
+            freqs[neighbor] += 1
             if reverse:
-                frequencies[reverse_complement(neighbor)] += 1
-    return frequencies
+                freqs[reverse_complement(neighbor)] += 1
+    return freqs
 
 
 def reverse_complement(genome):
-    '''
-    >>> reverse_complement('AAAACCCGGT')
-    'ACCGGGTTTT'
-
-    '''
     return genome.translate(COMPLEMENTS)[::-1]
 
 
 def start_positions(genome, pattern, *, dist_max=0, method=hamming_distance):
-    '''
-    >>> list(start_positions('GATATATGCATATACTT', 'ATAT'))
-    [1, 3, 9]
-    >>> genome = 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT'
-    >>> list(start_positions(genome, 'ATTCTGGA', dist_max=3))
-    [6, 7, 26, 27]
-
-    '''
     for i, substring in enumerate(window(genome, len(pattern))):
         if method(pattern, substring) <= dist_max:
             yield i
 
 
-def clumping(genome, kmer_length, window_size, min_freq):
-    '''
-    >>> genome = 'GATCAGCATAAGGGTCCCTGCAATGCATGACAAGCCTGCAGTTGTTTTAC'
-    >>> clumping(genome, 4, 25, 3)
-    {'TGCA'}
+def clumpers(genome, kmer_length, *, window_size=None, min_freq=0):
+    if window_size is None:
+        window_size = len(genome)
 
-    '''
-    freqs = dict_frequencies(genome[:window_size], kmer_length)
+    freqs = frequencies(genome[:window_size], kmer_length)
+
     patterns = {k for k, v in freqs.items() if v >= min_freq}
     for substring in window(genome, window_size+1):
         first_pattern = substring[:kmer_length]
@@ -231,37 +119,19 @@ def clumping(genome, kmer_length, window_size, min_freq):
 
         if freqs[last_pattern] >= min_freq:
             patterns.add(last_pattern)
+
     return patterns
 
 
 def skew(genome):
-    '''
-    >>> list(skew('CATGGGCATCGGCCATACGCC'))
-    [0, -1, -1, -1, 0, 1, 2, 1, 1, 1, 0, 1, 2, 1, 0, 0, 0, 0, -1, 0, -1, -2]
-
-    '''
     diff = 0
     yield diff
     for base in genome:
-        if base == 'C':
-            diff -= 1
-        elif base == 'G':
-            diff += 1
+        diff += -1 if base == 'C' else 1 if base == 'G' else 0
         yield diff
 
 
 def min_skew(genome):
-    '''
-    >>> genome = 'TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT'
-    >>> list(min_skew(genome))
-    [11, 24]
-
-    '''
     skew_list = list(skew(genome))
     minimum = min(skew_list)
     yield from (i for i, v in enumerate(skew_list) if v == minimum)
-
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
---
+++ tests/test_chapter_1.py
@@ -0,0 +1,192 @@
+from functools import partial
+import random
+
+import chapter_1 as c
+
+
+class TestDistance:
+    def test_hamming_distance(self):
+        s1 = 'GGGCCGTTGGT'
+        s2 = 'GGACCGTTGAC'
+        assert c.hamming_distance(s1, s2) == 3
+
+    def test_neighbors(self):
+        assert c.neighbors('ACG', 1) == {
+            'ACG', 'CCG', 'GCG', 'TCG',
+            'AAG', 'ACG', 'AGG', 'ATG',
+            'ACA', 'ACC', 'ACG', 'ACT',
+        }
+
+
+class TestPatternCount:
+    def setup(self):
+        genome = 'AACAAGCTGATAAACATTTAAAGAG'
+        pattern = 'AAAAA'
+        self.f = partial(c.pattern_count, genome, pattern)
+
+    def test_simple(self):
+        assert c.pattern_count('GCGCG', 'GCG') == 2
+        assert self.f() == 0
+
+    def test_mutable(self):
+        assert self.f(dist_max=1) == 4
+        assert self.f(dist_max=2) == 11
+
+
+class TestKmerCounts:
+    def setup(self):
+        self.genome = 'GCGCG'
+        self.kmer_length = 2
+        self.f = partial(c.kmer_counts, self.genome, self.kmer_length)
+
+    def test_simple(self):
+        assert self.f() == {
+            2: {'GC', 'CG'},
+        }
+
+    def test_mutable(self):
+        assert self.f(dist_max=1) == {
+            2: {'AC', 'GC', 'TC', 'GT', 'GA', 'TG', 'CG', 'CT', 'CA', 'AG'},
+            4: {'GG', 'CC'},
+        }
+
+    def test_mutable_reverse(self):
+        assert self.f(dist_max=1, reverse=True) == {
+            4: {'AC', 'GC', 'TC', 'GT', 'GA', 'TG', 'CG', 'CT', 'CA', 'AG'},
+            8: {'GG', 'CC'},
+        }
+
+
+class TestFrequentKmers:
+    def setup(self):
+        self.genome = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
+        self.kmer_length = 4
+        self.f = partial(c.frequent_kmers, self.genome, self.kmer_length)
+
+    def test_simple(self):
+        assert set(self.f()) == {'CATG', 'GCAT'}
+        assert set(self.f(min_freq=2)) == {'ATGA', 'CATG', 'GCAT', 'TGCA'}
+        assert set(self.f(min_freq=5)) == set()
+    
+    def test_mutable(self):
+        counts = c.kmer_counts(self.genome, self.kmer_length, dist_max = 1)
+        maximum = max(counts)
+        top_two = counts[maximum] | counts[maximum - 1]
+
+        assert set(self.f(dist_max=1)) == {'ATGC', 'ATGT', 'GATG'}
+        assert set(self.f(dist_max=1, min_freq=maximum - 1)) == top_two
+        assert set(self.f(dist_max=1, min_freq=maximum + 1)) == set()
+
+    def test_mutable_reverse(self):
+        assert set(self.f(dist_max=1, reverse=True)) == {'ACAT', 'ATGT'}
+
+
+class TestTransformations:
+    def test_pattern_to_number(self):
+        assert c.pattern_to_number('GT') == 11
+        assert c.pattern_to_number('AGT') == 11
+
+    def test_number_to_pattern(self):
+        assert c.number_to_pattern(11) == 'GT'
+        assert c.number_to_pattern(11, n_bases=3) == 'AGT'
+
+    def test_reverse_complement(self):
+        assert c.reverse_complement('AAAACCCGGT') == 'ACCGGGTTTT'
+
+        genome = ''.join(random.choices(c.BASES, k=random.randint(0, 100)))
+        assert c.reverse_complement(c.reverse_complement(genome)) == genome
+
+
+def test_window():
+    genome = 'GCGCG'
+    assert list(c.window(genome, 2)) == ['GC', 'CG', 'GC', 'CG']
+    assert list(c.window(genome, 3)) == ['GCG', 'CGC', 'GCG']
+
+
+class TestFrequencies:
+    def setup(self):
+        self.genome = 'AAGCAAAGGTGGG'
+        self.n_bases = 2
+        self.f = partial(c.frequencies, self.genome, self.n_bases)
+
+    def test_simple(self):
+        frequency_dict = self.f()
+        assert frequency_dict == {
+            'AA': 3, 'AG': 2,
+            'CA': 1,
+            'GC': 1, 'GG': 3, 'GT': 1,
+            'TG': 1,
+        }
+        assert c.vectorize(frequency_dict) == [
+            3, 0, 2, 0,
+            1, 0, 0, 0,
+            0, 1, 3, 1,
+            0, 0, 1, 0,
+        ]
+
+    def test_mutable(self):
+        frequency_dict = self.f(dist_max=1)
+        assert frequency_dict == {
+            'AA': 6, 'AC': 6, 'AG': 9, 'AT': 6,
+            'CA': 4, 'CC': 2, 'CG': 7, 'CT': 2,
+            'GA': 9, 'GC': 5, 'GG': 8, 'GT': 5,
+            'TA': 5, 'TC': 2, 'TG': 6, 'TT': 2,
+        }
+        assert c.vectorize(frequency_dict) == [
+            6, 6, 9, 6,
+            4, 2, 7, 2,
+            9, 5, 8, 5,
+            5, 2, 6, 2,
+        ]
+
+    def test_mutable_reverse(self):
+        frequency_dict = self.f(dist_max=1, reverse=True)
+        assert frequency_dict == {
+            'AA': 8, 'AC': 11, 'AG': 11, 'AT': 12,
+            'CA': 10, 'CC': 10, 'CG': 14, 'CT': 11,
+            'GA': 11, 'GC': 10, 'GG': 10, 'GT': 11,
+            'TA': 10, 'TC': 11, 'TG': 10, 'TT': 8,
+        }
+        assert c.vectorize(frequency_dict) == [
+            8, 11, 11, 12,
+            10, 10, 14, 11,
+            11, 10, 10, 11,
+            10, 11, 10, 8,
+        ]
+
+
+class TestSkew:
+    def test_skew(self):
+        genome = 'CATGGGCATCGGCCATACGCC'
+        assert list(c.skew(genome)) == [
+                   0, -1, -1, -1, 0, 1, 2, 1, 1, 1, 0,
+                   1, 2, 1, 0, 0, 0, 0, -1, 0, -1, -2
+                ]
+
+    def test_min_skew(self):
+        genome = 'TAAAGACTGCCGAGAGGCCAACACGAGTGCTAGAACGAGGGGCGTAAACGCGGGTCCGAT'
+        assert list(c.min_skew(genome)) == [11, 24]
+
+
+class TestStartPositions:
+    def test_simple(self):
+        assert list(c.start_positions('GATATATGCATATACTT', 'ATAT')) == [1, 3, 9]
+
+    def mutable(self):
+        genome = ('CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGT'
+                  'CAATCAAAT')
+        assert (list(c.start_positions(genome, 'ATTCTGGA', dist_max=3))
+                == [6, 7, 26, 27])
+
+
+class TestClumpers:
+    def setup(self):
+        self.genome = 'GATCAGCATAAGGGTCCCTGCAATGCATGACAAGCCTGCAGTTGTTTTAC'
+        self.kmer_length = 4
+        self.f = partial(c.clumpers, self.genome, self.kmer_length)
+
+    def test_full_window(self):
+        assert self.f(min_freq=3) == {'TGCA'}
+
+    def test_windowed(self):
+        assert self.f(window_size=25, min_freq=3) == {'TGCA'}
